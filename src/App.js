dcab7b259faf655e8e270344a5d24640ad9deb44
shaikh7010 <80522253+shaikh7010@users.noreply.github.com>
2024-08-25 22:47:17 +0530

    FS

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import React, { useState } from 'react';
-import Dashboard from './Dashboard'; // Adjust the path as needed
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState } from "react";
+// import Dashboard from './Dashboard'; // Adjust the path as needed
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
 
 const App = () => {
   const [isPopupOpen, setIsPopupOpen] = useState(false);
@@ -9,13 +12,19 @@ const App = () => {
   const closePopup = () => setIsPopupOpen(false);
 
   return (
-    <div>
-      {/* <button onClick={openPopup}>Open Dashboard</button> */}
+    // <Container fluid style={{height:'100%'}}>
+    //   <Dashboard />
+    //   {/* <button onClick={openPopup}>Open Dashboard</button> */}
 
-      {/* <Popup isOpen={isPopupOpen} onClose={closePopup}> */}
-        <Dashboard />
-      {/* </Popup> */}
-    </div>
+    //   {/* <Popup isOpen={isPopupOpen} onClose={closePopup}> */}
+
+    //   {/* </Popup> */}
+    // </Container>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 };
 
